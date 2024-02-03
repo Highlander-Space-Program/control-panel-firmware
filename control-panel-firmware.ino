89f8a7e6f7336a24d5408abd798f2e5f53275bb6
Charlie K <crknox412@gmail.com>
2024-02-02 18:15:01 -0800

    Changed buttons to output on rising edge instead of falling edge

--- control-panel-firmware.ino
+++ control-panel-firmware.ino
@@ -77,67 +77,67 @@ EdgeDetector ABORT_EDGE_DETECTOR;
 void loop() {
   // Edge Detection for START_SEQ_1 Button | Pin 2
   START_SEQ_1_EDGE_DETECTOR.update(!digitalRead(START_SEQ_1));
-  if (START_SEQ_1_EDGE_DETECTOR.hasFallen()) {
+  if (START_SEQ_1_EDGE_DETECTOR.hasRisen()) {
     Serial.write(START_SEQUENCE_1);
   }
 
   // Edge Detection for FILL_SEQ_1 Button | Pin 3
   FILL_SEQ_1_EDGE_DETECTOR.update(!digitalRead(FILL_SEQ_1));
-  if (FILL_SEQ_1_EDGE_DETECTOR.hasFallen()) {
+  if (FILL_SEQ_1_EDGE_DETECTOR.hasRisen()) {
     Serial.write(FILL_SEQUENCE_1);
   }
 
   // Edge Detection for FILL_SEQ_2 Button | Pin 4
   FILL_SEQ_2_EDGE_DETECTOR.update(!digitalRead(FILL_SEQ_2));
-  if (FILL_SEQ_2_EDGE_DETECTOR.hasFallen()) {
+  if (FILL_SEQ_2_EDGE_DETECTOR.hasRisen()) {
     Serial.write(FILL_SEQUENCE_2);
   }
 
   // Edge Detection for FILL_SEQ_3 Button | Pin 5
   FILL_SEQ_3_EDGE_DETECTOR.update(!digitalRead(FILL_SEQ_3));
-  if (FILL_SEQ_3_EDGE_DETECTOR.hasFallen()) {
+  if (FILL_SEQ_3_EDGE_DETECTOR.hasRisen()) {
     Serial.write(FILL_SEQUENCE_3);
   }
 
   // Edge Detection for NOS_FV_08 Button | Pin 6
   NOS_FV_08_EDGE_DETECTOR.update(!digitalRead(NOS_FV_08));
-  if (NOS_FV_08_EDGE_DETECTOR.hasFallen()) {
+  if (NOS_FV_08_EDGE_DETECTOR.hasRisen()) {
     Serial.write(NOS_VALVE_2_TOGGLE);
   }
 
   // Edge Detection for NOS_FV_04 Button | Pin 7
   NOS_FV_04_EDGE_DETECTOR.update(!digitalRead(NOS_FV_04));
-  if (NOS_FV_04_EDGE_DETECTOR.hasFallen()) {
+  if (NOS_FV_04_EDGE_DETECTOR.hasRisen()) {
     Serial.write(NOS_VALVE_1_TOGGLE);
   }
 
   // Edge Detection for N2_FV_02 Button | Pin 8
   N2_FV_02_EDGE_DETECTOR.update(!digitalRead(N2_FV_02));
-  if (N2_FV_02_EDGE_DETECTOR.hasFallen()) {
+  if (N2_FV_02_EDGE_DETECTOR.hasRisen()) {
     Serial.write(N2_VALVE_TOGGLE);
   }
 
   // Edge Detection for ETOH_FV_03 Button | Pin 9
   ETOH_FV_03_EDGE_DETECTOR.update(!digitalRead(ETOH_FV_03));
-  if (ETOH_FV_03_EDGE_DETECTOR.hasFallen()) {
+  if (ETOH_FV_03_EDGE_DETECTOR.hasRisen()) {
     Serial.write(ETOH_FLOW_VALVE_TOGGLE);
   }
 
   // Edge Detection for SHUT_DOWN Button | Pin 10
   SHUT_DOWN_EDGE_DETECTOR.update(!digitalRead(SHUT_DOWN));
-  if (SHUT_DOWN_EDGE_DETECTOR.hasFallen()) {
+  if (SHUT_DOWN_EDGE_DETECTOR.hasRisen()) {
     Serial.write(CLOSE_ALL);
   }
 
   // Edge Detection for IGNITE_START Button | Pin 11
   IGNITE_START_EDGE_DETECTOR.update(!digitalRead(IGNITE_START));
-  if (IGNITE_START_EDGE_DETECTOR.hasFallen()) {
+  if (IGNITE_START_EDGE_DETECTOR.hasRisen()) {
     Serial.write(IGNITE);
   }
 
   // Edge Detection for ABORT Button | Pin 12
   ABORT_EDGE_DETECTOR.update(!digitalRead(ABORT_CMD));
-  if (ABORT_EDGE_DETECTOR.hasFallen()) {
+  if (ABORT_EDGE_DETECTOR.hasRisen()) {
     Serial.write(ABORT);
   }
 
