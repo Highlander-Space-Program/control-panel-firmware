2049ec89533d2a0db17c747c5a1a2a9b411c0b2a
Charlie K <crknox412@gmail.com>
2024-02-26 16:35:58 -0800

    Turn on LEDs based on serial value

--- control-panel-firmware.ino
+++ control-panel-firmware.ino
@@ -38,59 +38,72 @@ const int CLOSE_ALL_S = 14;
 const int ACTIVATE_IGNITER_S = 15;
 const int ACTIVATE_SERVOS_S = 17;
 
-// Button
+// LEDs
+const int NOS2_OPENED_L = 18;
+const int NOS1_OPENED_L = 19;
+const int N2_OPENED_L = 20;
+const int ETOH_OPENED_L = 21;
+const int IGNITE_ACTIVATED_L = 22;
 
+// Serial Check
+uint8_t receiveAck = 0x00;
+uint8_t bitMask = 0x01;
 
 // enums
 enum COMMANDS { 
-  OPEN_NOS2 = 0,
-  CLOSE_NOS2 = 1,
-  OPEN_NOS1 = 2,
-  CLOSE_NOS1 = 3,
-  OPEN_N2 = 4,
-  CLOSE_N2 = 5,
-  OPEN_ETOH = 6,
-  CLOSE_ETOH = 7,
-  START_1 = 8,
-  FILL_1 = 9,
-  FILL_2 = 10,
-  FILL_3 = 11,
-  CLOSE_ALL = 12,
-  DECLOSE_ALL = 13,
-  ACTIVATE_IGNITER = 14,
+  OPEN_NOS2 = 2,
+  CLOSE_NOS2 = 3,
+  OPEN_NOS1 = 4,
+  CLOSE_NOS1 = 5,
+  OPEN_N2 = 6,
+  CLOSE_N2 = 7,
+  OPEN_ETOH = 8,
+  CLOSE_ETOH = 9,
+  START_1 = 10,
+  FILL_1 = 11,
+  FILL_2 = 12,
+  FILL_3 = 13,
+  CLOSE_ALL = 14,
+  DECLOSE_ALL = 14,
+  ACTIVATE_IGNITER = 15,
   DEACTIVATE_IGNITER = 15,
   ABORT = 16,
   ACTIVATE_SERVOS = 17,
-  DEACTIVATE_SERVOS = 18,
-  DEABORT = 19,
-  CHECK_STATE = 20
+  DEACTIVATE_SERVOS = 17,
+  DEABORT = 16,
+  CHECK_STATE = 18
 };
 
 void setup() {
   Serial.begin(9600);
 
-  // Initialize buttons as INPUTs
-  pinMode(OPEN_NOS2, INPUT);
-  pinMode(CLOSE_NOS2, INPUT);
-  pinMode(OPEN_NOS1, INPUT);
-  pinMode(CLOSE_NOS1, INPUT);
-  pinMode(OPEN_N2, INPUT);
-  pinMode(CLOSE_N2, INPUT);
-  pinMode(OPEN_ETOH, INPUT);
-  pinMode(CLOSE_ETOH, INPUT);
-  pinMode(START_1, INPUT);
-  pinMode(FILL_1, INPUT);
-  pinMode(FILL_2, INPUT);
-  pinMode(FILL_3, INPUT);
-  pinMode(CLOSE_ALL, INPUT);
-  pinMode(DECLOSE_ALL, INPUT);
-  pinMode(ACTIVATE_IGNITER, INPUT);
-  pinMode(DEACTIVATE_IGNITER, INPUT);
-  pinMode(ABORT, INPUT);
-  pinMode(ACTIVATE_SERVOS, INPUT);
-  pinMode(DEACTIVATE_SERVOS, INPUT);
-  pinMode(DEABORT, INPUT);
-  pinMode(CHECK_STATE, INPUT);
+  // Initialize Buttons as INPUTs
+  pinMode(OPEN_NOS2_B, INPUT);
+  pinMode(CLOSE_NOS2_B, INPUT);
+  pinMode(OPEN_NOS1_B, INPUT);
+  pinMode(CLOSE_NOS1_B, INPUT);
+  pinMode(OPEN_N2_B, INPUT);
+  pinMode(CLOSE_N2_B, INPUT);
+  pinMode(OPEN_ETOH_B, INPUT);
+  pinMode(CLOSE_ETOH_B, INPUT);
+  pinMode(START_1_B, INPUT);
+  pinMode(FILL_1_B, INPUT);
+  pinMode(FILL_2_B, INPUT);
+  pinMode(FILL_3_B, INPUT);
+  pinMode(ABORT_B, INPUT);
+  pinMode(CHECK_STATE_B, INPUT);
+
+  // Initialize Switches as INPUTs
+  pinMode(CLOSE_ALL_S, INPUT);
+  pinMode(ACTIVATE_IGNITER_S, INPUT);
+  pinMode(ACTIVATE_SERVOS_S, INPUT);
+
+  // Initialize LEDs
+  pinMode(NOS2_OPENED_L, OUTPUT);
+  pinMode(NOS1_OPENED_L, OUTPUT);
+  pinMode(N2_OPENED_L, OUTPUT);
+  pinMode(ETOH_OPENED_L, OUTPUT);
+  pinMode(IGNITE_ACTIVATED_L, OUTPUT);
 }
 
 // Button Edge Detectors
@@ -115,6 +128,47 @@ EdgeDetector ACTIVATE_IGNITER_EDGE_DETECTOR;
 EdgeDetector ACTIVATE_SERVOS_EDGE_DETECTOR;
 
 void loop() {
+  // Check if there are open valves
+  if (Serial.available() > 0) {
+    // Receive serial value
+    receiveAck = Serial.read();
+
+    bitMask = 0x01;
+    // Check serial bits to see which valves are open
+    // and turn on corresponding LED
+    while (bitMask != 0x20)
+    {
+      if (receiveAck & bitMask && bitMask == 0x01)
+      {
+        digitalWrite(NOS2_OPENED_L, HIGH);
+      }
+      bitMask = bitMask << 1;
+
+      if (receiveAck & bitMask && bitMask == 0x02)
+      {
+        digitalWrite(NOS1_OPENED_L, HIGH);
+      }
+      bitMask = bitMask << 1;
+      
+      if (receiveAck & bitMask && bitMask == 0x04)
+      {
+        digitalWrite(N2_OPENED_L, HIGH);
+      }
+      bitMask = bitMask << 1;
+      
+      if (receiveAck & bitMask && bitMask == 0x08)
+      {
+        digitalWrite(ETOH_OPENED_L, HIGH);
+      }
+      bitMask = bitMask << 1;
+
+      if (receiveAck & bitMask && bitMask == 0x10)
+      {
+        digitalWrite(IGNITE_ACTIVATED_L, HIGH);
+      }
+    }
+  }
+
   // Buttons
 
   // Edge Detection for OPEN_NOS2 Button | Pin 2
